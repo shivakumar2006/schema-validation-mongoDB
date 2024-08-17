ff034aef4a823bce1fac543771df023e8df59bf4
shivakumar2006 <official.shivakumar2006@gmail.com>
2024-08-17 23:00:35 +0530

    right commit

--- temp.js
+++ temp.js
@@ -1,19 +1,38 @@
-db.createCollection("nonFiction, {
+// db.createCollection("nonFiction, {
+//     validator: {
+//         $jsonSchema: {
+//             required:["name", "price"],
+//             properties:{
+//                 name:{
+//                     bsonType: "string"
+//                     description: "must be a string and required"
+//                 },
+
+//                 price:{
+//                     bsonTyoe: "number"
+//                     description: "must be a number and required"
+//                 }
+//             }
+//         }
+//     },
+//     validationAction: "error" 
+// })
+
+db.createCollection("nonFiction", {
     validator: {
         $jsonSchema: {
-            required:["name", "price"],
-            properties:{
-                name:{
-                    bsonType: "string"
-                    description: "must be a string and required"
+            required: ["name", "price"],
+                properties: {
+                name: {
+                    bsonType: "string",
+                    description: "must be a string and required",
                 },
-
-                price:{
-                    bsonTyoe: "number"
-                    description: "must be a number and required"
+                price: {
+                    bsonTyoe: "number",
+                    description: "mudt be a number and required",
                 }
-            }
+                }
         }
     },
-    validationAction: "error" 
+    validationAction: "error"
 })
