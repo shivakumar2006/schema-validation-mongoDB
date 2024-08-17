815abfc9ed617bfc82d3339fdbf1d1b3a8a4f567
shivakumar2006 <official.shivakumar2006@gmail.com>
2024-08-17 22:46:39 +0530

    second commit

---
+++ temp.js
@@ -0,0 +1,19 @@
+db.createCollection("nonFiction, {
+    validator: {
+        $jsonSchema: {
+            required:["name", "price"],
+            properties:{
+                name:{
+                    bsonType: "string"
+                    description: "must be a string and required"
+                },
+
+                price:{
+                    bsonTyoe: "number"
+                    description: "must be a number and required"
+                }
+            }
+        }
+    },
+    validationAction: "error" 
+})
