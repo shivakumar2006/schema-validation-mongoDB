24c9777206a0e0ebd8cadc36a605c2979316c14b
shivakumar2006 <official.shivakumar2006@gmail.com>
2024-08-17 23:42:19 +0530

    commit

--- temp.js
+++ temp.js
@@ -21,7 +21,7 @@
 db.createCollection("nonFiction", {
     validator: {
         $jsonSchema: {
-            required: ["name", "price", "author"],
+            required: ["name", "price", "authors"],
                 properties: {
                 name: {
                     bsonType: "string",
@@ -31,9 +31,57 @@ db.createCollection("nonFiction", {
                     bsonType: "number",
                     description: "must be a number and required",
                 },
-                author: {
+                authors: {
                     bsonType: "array",
                     description: "must be a array and required",
+                    items: {
+                        bsonType: "object",
+                        required: ["name", "object"],
+                            properties: {
+                                name: {
+                                    bsonType: "string",
+                                },
+                                emnail: {
+                                    bsonType: "string"
+                                }
+                            }
+                    }
+                },
+                }
+        }
+    },
+    validationAction: "error"
+})
+
+db.runCommand({
+    collMod: "nonFiction",
+    validator: {
+        $jsonSchema: {
+            required: ["name", "price", "authors"],
+                properties: {
+                name: {
+                    bsonType: "string",
+                    description: "must be a string and required",
+                },
+                price: {
+                    bsonType: "number",
+                    description: "must be a number and required",
+                },
+                authors: {
+                    bsonType: "array",
+                    description: "must be a array and required",
+                    items: {
+                        bsonType: "object",
+                        required: ["name", "email"],
+                            properties: {
+                                name: {
+                                    bsonType: "string",
+                                },
+                                email: {
+                                    bsonType: "string"
+                                }
+                            }
+                    }
                 },
                 }
         }
